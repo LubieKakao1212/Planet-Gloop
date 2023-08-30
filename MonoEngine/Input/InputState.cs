c64272964345d33a4c58778d7234fa3c07a65578
LubieKakao1212 <grzesio.go@gmail.com>
2023-08-30 15:53:53 +0200

    InputManager (untested)

---
+++ MonoEngine/Input/InputState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoEngine.Input
+{
+    public enum InputState
+    {
+        Idle = 0,
+        Started = 1,
+        Performed = 2,
+        Canceled = 3
+    }
+}
