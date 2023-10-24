f6ba00aa5b563026b7ca1a3a5014055c180ed16b
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-10-24 21:35:15 +0200

    turret collision

--- GlobalLoopGame/Planet/PlanetObject.cs
+++ GlobalLoopGame/Planet/PlanetObject.cs
@@ -38,6 +38,7 @@ namespace GlobalLoopGame.Planet
             fixture.CollidesWith = Category.None;
             fixture.CollidesWith |= Category.Cat1;
             fixture.CollidesWith |= Category.Cat2;
+            fixture.CollidesWith |= Category.Cat3;
         }
 
         public void ModifyHealth(int healthModification)
